72be8cb6056979bd2e437a0ca4fb5433b78bdd3b
FatmaDuran <fatmaadrnn@gmail.com>
2021-04-25 14:56:16 +0300

    mvc proje kampı
    1.aşama

---
+++ MvcProjeKampi/DataAccessLayer/Migrations/202104241235173_mig1.cs
@@ -0,0 +1,30 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Contents", "HeadingID", "dbo.Headings");
+            DropIndex("dbo.Contents", new[] { "HeadingID" });
+            RenameColumn(table: "dbo.Contents", name: "Writer_WriterID", newName: "WriterID");
+            RenameIndex(table: "dbo.Contents", name: "IX_Writer_WriterID", newName: "IX_WriterID");
+            AlterColumn("dbo.Contents", "HeadingID", c => c.Int(nullable: false));
+            CreateIndex("dbo.Contents", "HeadingID");
+            AddForeignKey("dbo.Contents", "HeadingID", "dbo.Headings", "HeadingID", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Contents", "HeadingID", "dbo.Headings");
+            DropIndex("dbo.Contents", new[] { "HeadingID" });
+            AlterColumn("dbo.Contents", "HeadingID", c => c.Int());
+            RenameIndex(table: "dbo.Contents", name: "IX_WriterID", newName: "IX_Writer_WriterID");
+            RenameColumn(table: "dbo.Contents", name: "WriterID", newName: "Writer_WriterID");
+            CreateIndex("dbo.Contents", "HeadingID");
+            AddForeignKey("dbo.Contents", "HeadingID", "dbo.Headings", "HeadingID");
+        }
+    }
+}
